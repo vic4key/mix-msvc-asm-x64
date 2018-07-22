353bd16fd161263f52db4feb7bd3589f3f9661d9
Vic P <vic4key@gmail.com>
2018-07-23 00:15:12 +0700

    : Allocate the shadow space on the stack.

--- Sample/Assembly.asm
+++ Sample/Assembly.asm
@@ -1,5 +1,17 @@
 ; ASSEMBLE CODE
 
+; https://msdn.microsoft.com/en-us/library/ms235286.aspx
+; http://www.tortall.net/projects/yasm/manual/html/objfmt-win64-exception.html
+; https://en.wikipedia.org/wiki/X86_calling_conventions#Microsoft_x64_calling_convention
+
+; 1. THE STACK POINTER MUST BE ALIGNED TO 16 BYTES IN ANY REGION OF CODE.
+
+; 2. STACK SPACE IS ALLOCATED FOR THE FOUR REGISTER PARAMETERS "SHADOW SPACE". SHADOW SPACE MUST BE ALLOCATED FOR FOUR
+; REGISTER PARAMETERS EVEN IF THE CALLED FUNCTION DOES NOT HAVE THIS MANY PARAMETERS.
+
+; Basically, a function without no parameter, always used at least 4 registers * 8 bytes for
+; shadow space + 8 bytes = 40 bytes, but after adjusted following the stack alignment it will be 48 bytes = 30h bytes.
+
 ; Exportation
 PUBLIC F1
 PUBLIC F2
@@ -18,7 +30,7 @@ TXT_F2 db "I am F2() from asm code.",0
 F1 PROC
   PUSH RBP
   MOV RBP, RSP
-  SUB RSP, 40
+  SUB RSP, 30H
   LEA RCX, TXT_F1
   CALL puts
   LEAVE
@@ -28,7 +40,7 @@ F1 ENDP
 F2 PROC
   PUSH RBP
   MOV RBP, RSP
-  SUB RSP, 40
+  SUB RSP, 30H
   LEA RCX, TXT_F2
   CALL puts
   LEAVE
